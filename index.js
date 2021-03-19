d2fb1bfdd4b17d3d20849507cd290b158e8189c6
calvin <calvin.francis96@outlook.com>
2021-03-19 14:57:56 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+// Removing main with id of 'main' from the DOM
+const main = document.querySelector('#main');
+main.remove();
+
+//Adding h1 element to the DOM and assigning it to 'newHeader' variable
+const newHeader = document.createElement('h1');
+//assigning id of victory to newHeader
+newHeader.id = "victory";
+
+//Adding content to newHeader using innerHTML
+newHeader.innerHTML = "Calvin is the champion";
+document.body.append(newHeader);
